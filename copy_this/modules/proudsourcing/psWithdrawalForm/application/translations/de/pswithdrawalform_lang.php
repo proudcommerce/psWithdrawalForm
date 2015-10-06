e3e9826fb114ce91c0202eb7c58033a7a9b725b8
kristianhempel <kh@shopmodule.com>
2015-10-06 10:40:57 +0200

    added: translation support for 4.7.x to 4.9.x

---
+++ copy_this/modules/proudsourcing/psWithdrawalForm/application/translations/de/pswithdrawalform_lang.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @copyright (c) Proud Sourcing GmbH | 2014
+ * @link www.proudcommerce.com
+ * @package psWithdrawalForm
+ * @version 1.1.3
+ **/
+$sLangName = 'Deutsch';
+$aLang     = array(
+    'charset'                                   => 'UTF-8',
+
+    'PSWDF_SUBJECT'                             => 'Widerruf',
+    'PSWDF_SUBJECT_CUSTOMER'                    => 'Eingangsbestaetigung Widerruf',
+    'PSWDF_TITLE'                               => 'Widerrufsformular',
+    'PSWDF_MSG1'                                => 'Wenn Sie den Kaufvertrag widerrufen möchten, dann füllen Sie bitte folgendes Formular vollständig aus.',
+    'PSWDF_MSG2'                                => 'Hiermit widerrufe(n) ich/wir den von mir/uns abgeschlossenen Vertrag über den Kauf der folgenden Artikel.',
+    'PSWDF_MSG3'                                => 'Informationen zu Ihrem Widerrufsrecht finden Sie',
+    'PSWDF_MSG4'                                => 'an dieser Stelle',
+    'PSWDF_MSG5'                                => '.',
+    'PSWDF_TO'                                  => 'Empfänger:<br><br><br>',
+    'PSWDF_SEND'                                => 'Widerruf absenden',
+    'PSWDF_ORDERNR'                             => 'Bestellnummer:',
+    'PSWDF_DATE_ORDER'                          => 'bestellt am:',
+    'PSWDF_DATE_RECEIVED'                       => 'erhalten am:',
+    'PSWDF_DATE_ARTICLES'                       => 'Artikel:',
+    'PSWDF_NAME'                                => 'Name:',
+    'PSWDF_ADDRESS'                             => 'Anschrift:',
+    'PSWDF_EMAIL'                               => 'eMail-Adresse:',
+    'PSWDF_DATE_DRAWAL'                         => 'Datum des Widerrufs:',
+    'PSWDF_MSG_RECEIVED'                        => '<span style="color: green;"><b>Wir haben Ihren Widerruf erhalten und senden Ihnen per eMail eine Bestätigung.</b></span>',
+);
